c5396b7f847b4b98c77f9d89c4357c177117050f
Коннов Кирилл <konnov_k@protonmail.com>
2023-01-08 23:58:55 +0300

    Изменил реализацию jwt

--- api/jwt.py
+++ api/jwt.py
@@ -1,49 +1,153 @@
+import random
 import time
 
+from aiohttp import web
 import jwt
-from jwt import DecodeError
+from jwt import PyJWTError
+from loguru import logger
+
+from db.models.user import User
 
 JWT_SECRET = 'jwt_secret_tjli4k5ngiuhgze-hb8o65jh'
 
 
-def create_jwt(email: str, jwt_secret: str = JWT_SECRET) -> (str, str, int):
-    now = int(time.time())
-    expires_in = now + 60 * 20  # + 20 minutes
-    refresh_token = jwt.encode({
-        'email': email,
-    }, jwt_secret, algorithm="HS256")
-    access_token = jwt.encode({
-        'email': email,
-        'refresh_token': refresh_token,
-        'expires_in': expires_in
-    }, jwt_secret, algorithm="HS256")
-    return access_token, refresh_token, expires_in
+class JWTException(Exception):
+    """
+    Исключение, которое выбрасивают методы, работающие с jwt.
+    """
+    pass
 
 
-def refresh_jwt(access_token: str, refresh_token: str, jwt_secret: str = JWT_SECRET) -> (str, str, int):
-    access_token_data = jwt.decode(access_token, jwt_secret, algorithms=["HS256"])
-    access_token_email = access_token_data['email']
-    if access_token_data['refresh_token'] == refresh_token:
-        return create_jwt(access_token_email, jwt_secret)
-    else:
-        return None, None, None
+class JWT:
+    """
+    Класс для работы с jwt токенами.
+    """
+    def __init__(self):
+        self.JWT_SECRET = ''.join(random.choice('qwertyuiopasdfghjklzxcvbnm123456789') for _ in range(128))
+        self.TOKEN_TTL_SECONDS = 60 * 20
+
+    def __encode_jwt(self, data: dict) -> str:
+        try:
+            jwt_secret = self.JWT_SECRET
+            return jwt.encode(data, jwt_secret, algorithm="HS256")
+        except PyJWTError:
+            raise JWTException
+
+    def __decode_jwt(self, token: str) -> dict:
+        try:
+            jwt_secret = self.JWT_SECRET
+            return jwt.decode(token, jwt_secret, algorithms=["HS256"])
+        except PyJWTError as err:
+            raise JWTException
+
+    def create_jwt(self, email: str) -> (str, str, int):
+        """
+        Создает access token и refresh token.
+        :raise JWTException: если случилась ошибка при создании токенов
+        :param email: email аутентифицируемого пользователя
+        :return: тройка (access_token, refresh_token, expires_in)
+        """
+        now = int(time.time())
+        expires_in = now + self.TOKEN_TTL_SECONDS
+        access_token = self.__encode_jwt({
+            'email': email,
+            'created_in': now,
+            'expires_in': expires_in
+        })
+        refresh_token = self.__encode_jwt({
+            'email': email,
+            'created_in': now,
+            'access_token': access_token
+        })
+        return access_token, refresh_token, expires_in
+
+    def refresh_jwt(self, access_token: str, refresh_token: str) -> (str, str, int):
+        """
+        Обновляет access token и refresh token.
+        :raise JWTException: если токены некорректные
+        :param access_token: access token
+        :param refresh_token: refresh token
+        :return: тройка (access_token, refresh_token, expires_in)
+        """
+        access_token_data = self.__decode_jwt(access_token)
+        refresh_token_data = self.__decode_jwt(refresh_token)
+        try:
+            refresh_token_decoded_access_token = refresh_token_data['access_token']
+            refresh_token_created_in = refresh_token_data['created_in']
+            access_token_created_in = access_token_data['created_in']
+            refresh_token_email = refresh_token_data['email']
+            access_token_email = access_token_data['email']
+        except ValueError:
+            raise JWTException
+        if (
+                refresh_token_decoded_access_token == access_token and
+                refresh_token_created_in == access_token_created_in and
+                refresh_token_email == access_token_email
+        ):
+            return self.create_jwt(access_token_email)
+        else:
+            raise JWTException
+
+    def check_access_token(self, access_token: str) -> bool:
+        """
+        Проверяет, жив ли еще токен.
+        :raise JWTException: если токен некорректный
+        :param access_token: токен
+        :return: True - если токен еще жив, False - иначе
+        """
+        access_token_data = self.__decode_jwt(access_token)
+        try:
+            access_token_expires_in = access_token_data['expires_in']
+        except ValueError:
+            raise JWTException
+        now = int(time.time())
+        if now > access_token_expires_in:
+            return False
+        return True
+
+    def get_email_from_access_token(self, access_token: str) -> str:
+        access_token_data = self.__decode_jwt(access_token)
+        try:
+            access_token_email = access_token_data['email']
+        except ValueError:
+            raise JWTException
+        return access_token_email
 
 
-def check_access_token(access_token: str, jwt_secret: str = JWT_SECRET) -> bool:
-    try:
-        access_token_data = jwt.decode(access_token, jwt_secret, algorithms=["HS256"])
-    except Exception:
-        return False
-    access_token_expires_in = access_token_data['expires_in']
-    now = int(time.time())
-    if now > access_token_expires_in:
-        return False
-    return True
+def jwt_middleware(handler):
+    """
+    Собственный промежуточный обработчик (декоратор) для проверки jwt токена.
+    :param handler: обработчик запроса
+    """
+    async def wrapper(request: web.Request):
+        logger.debug(f'JWT Check')
+        logger.debug(request.url.path)
+        logger.debug(f"HEADER: Authorization: {request.headers.get('Authorization')}")
+        if not request.headers.get('Authorization'):
+            raise web.HTTPUnauthorized()
+        if request.headers.get('Authorization').split(' ')[0] != 'Bearer':
+            raise web.HTTPUnauthorized()
+        try:
+            access_token = request.headers.get('Authorization').split(' ')[1]
+        except KeyError:
+            raise web.HTTPUnauthorized()
+        try:
+            check_access_token = request.app['jwt'].check_access_token(access_token)
+        except JWTException:
+            raise web.HTTPUnauthorized()
+        if not check_access_token:
+            raise web.HTTPUnauthorized()
+        try:
+            user_email = request.app['jwt'].get_email_from_access_token(access_token)
+        except JWTException:
+            raise web.HTTPUnauthorized()
 
+        async with request.app['db'].begin() as conn:
+            if not await User.exists(conn, user_email):
+                raise web.HTTPUnauthorized()
 
-def get_email_from_access_token(access_token: str, jwt_secret: str = JWT_SECRET) -> str | None:
-    try:
-        access_token_data = jwt.decode(access_token, jwt_secret, algorithms=["HS256"])
-    except DecodeError:
-        return None
-    return access_token_data['email']
+        request.app['email'] = user_email
+        logger.debug(f'JWT Ok, email = {user_email}')
+        response = await handler(request)
+        return response
+    return wrapper
