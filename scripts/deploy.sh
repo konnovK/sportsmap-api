b002069b1500b24f35bf001f3ce39ac45701489f
konnovK <konnov_k@protonmail.com>
2023-01-18 23:36:45 +0000

    делаю деплой

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 echo "${OAUTH}" | docker login --username oauth --password-stdin cr.yandex
 
-docker build --build-arg API_PORT=${API_PORT} --tag=cr.yandex/${REGISTRY_ID}/sportsmap-backend-new:v${VERSION} ./api/
+docker build --build-arg API_PORT=${API_PORT} --tag=cr.yandex/${REGISTRY_ID}/sportsmap-backend-new:v${VERSION} ../api/
 
 docker push cr.yandex/${REGISTRY_ID}/sportsmap-backend-new:v${VERSION}
