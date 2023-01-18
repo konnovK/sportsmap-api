d2442af59b101a057312b5eee41895272209a94a
krendeleno <krendeleno@gmail.com>
2023-01-18 23:08:52 +0300

    ci: скрипты для деплоя

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo "${TOKEN}" | docker login --username oauth --password-stdin cr.yandex
+echo "${OAUTH}" | docker login --username oauth --password-stdin cr.yandex
 
 docker build --build-arg API_PORT=${API_PORT} --tag=cr.yandex/${REGISTRY_ID}/sportsmap-backend-new:v${VERSION} ./api/
 
